b452bb09483e31877579d6e8921d294d79660347
Enrico R <enricorrx@gmail.com>
2023-09-21 11:29:56 +0200

    fix bugs and refactor

--- Seq2Vec_train&test.py
+++ Seq2Vec_train&test.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 
 # In[ ]:
@@ -12,46 +11,65 @@ import keras
 from keras.backend.tensorflow_backend import set_session
 import ast
 import numpy as np
-os.chdir('dir') # direct
+
+os.chdir('train')  # direct
 
 # construct training dataset
-m=[]
-g=open('train_data','r')
-lines=g.readlines()
+m = []
+g = open('train-sample.csv', 'r')
+lines = g.readlines()
 for line in lines:
-    n=line[:-1]
-    k = ast.literal_eval(n)
-    j=list(k)
-    m.append(j) 
+    n = line[:-2]
+    if n == "":
+        continue
+    try:
+        k = ast.literal_eval(n)
+        j = list(k)
+        m.append(j)
+    except ValueError:
+        print(f"Can't evaluate: {n}", flush=False)
+    except TypeError:
+        print(f"Can't iterate: {n}", flush=False)
 g.close()
 train_x = m
 
 # construct testing dataset
-a=[]
-f=open('test_data','r')
-lines=f.readlines()
+a = []
+f = open('test-sample.csv', 'r')
+lines = f.readlines()
 for line in lines:
-    b=line[:-1]
-    c = ast.literal_eval(b)
-    d=list(c)
-    a.append(d)
+    b = line[:-2]
+    if b == "":
+        continue
+    try:
+        c = ast.literal_eval(b)
+        d = list(c)
+        a.append(d)
+    except ValueError:
+        print(f"Can't evaluate: {b}", flush=False)
+    except TypeError:
+        print(f"Can't iterate: {b}", flush=False)
 f.close()
 test_x = a
 
 # construct testing labels
-test_y=[]
-for i in range(0,500):
+test_y = []
+num_train_virus = 99815
+num_train_host = 96963
+num_test_virus = 99892
+num_test_host = 99997
+for i in range(num_train_virus):
     test_y.append(1)
-for i in range(500,1000):
+for i in range(num_train_host):
     test_y.append(0)
 
 # construct training labels
-train_y=[]
-for i in range(0,4500):
+train_y = []
+for i in range(num_test_virus):
     train_y.append(1)
-for i in range(4500,9000):
+for i in range(num_test_host):
     train_y.append(0)
-    
+
 # vocabulary length
 VOCAB_LEN = 65
 # max sequence length
@@ -70,15 +88,14 @@ net = tflearn.input_data([None, SEQUENCE_LEN])
 net = tflearn.embedding(net, input_dim=VOCAB_LEN, output_dim=WORD_FEATURE_DIM)
 net = tflearn.lstm(net, DOC_FEATURE_DIM, dropout=0.8)
 net = tflearn.fully_connected(net, 2, activation='softmax')
-net = tflearn.regression(net, optimizer='adam', learning_rate=0.0002,loss='categorical_crossentropy')
-model = tflearn.DNN(net, tensorboard_verbose=0,tensorboard_dir="/tmp/tflearn_logs/")
+net = tflearn.regression(net, optimizer='adam', learning_rate=0.0002, loss='categorical_crossentropy')
+model = tflearn.DNN(net, tensorboard_verbose=0, tensorboard_dir="/tmp/tflearn_logs/")
 
 config = tf.ConfigProto()
-config.gpu_options.allocator_type = 'BFC' 
+config.gpu_options.allocator_type = 'BFC'
 config.gpu_options.per_process_gpu_memory_fraction = 0.3
 config.gpu_options.allow_growth = True
 set_session(tf.Session(config=config))
 
 # training
-model.fit(train_x, train_y, validation_set=(test_x, test_y), show_metric=True,n_epoch=25, batch_size=256)
-
+model.fit(train_x, train_y, validation_set=(test_x, test_y), show_metric=True, n_epoch=25, batch_size=256)
